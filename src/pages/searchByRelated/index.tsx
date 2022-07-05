351cf0827feb9629a19be4b4dd955c41c4138a07
Gabriel4420 <gabriel_rodrigues_perez@hotmail.com>
2022-07-05 00:15:08 -0300

    arrumando query

--- src/pages/searchByRelated/index.tsx
+++ src/pages/searchByRelated/index.tsx
@@ -12,14 +12,15 @@ export function SearchByRelated() {
     e.preventDefault()
     try {
       const resp = await api.get(
-        `search/repositories?q=${repoName}+user:${username}&per_page=10`,
+        `/search/repositories?q=${repoName}+user%3A${username}`,
         {
           headers: {
+            Accept: 'application/vnd.github+json',
             Authorization: `token ${import.meta.env.VITE_APP_GITHUB_TOKEN}`,
           },
         },
       )
-
+      console.log(resp.data)
       setResponseRepo(resp.data.items)
     } catch (error) {
       console.log(error)
@@ -28,12 +29,13 @@ export function SearchByRelated() {
 
   return (
     <>
-      <div>
-        <h1 className="font-sans text-center uppercase w-full font-semibold text-indigo-800 lg:text-xl md:text-lg sm:text-[8px] p-10">
-          Busca por Repositórios Relacionados
-        </h1>
+      <h1 className="font-sans text-center uppercase w-full font-semibold text-indigo-800 lg:text-xl md:text-lg sm:text-[8px] p-10">
+        Busca por Repositórios Relacionados
+      </h1>
+
+      {responseRepo?.length > 0 ? (
         <form
-          className="flex flex-col px-28 h-[490px] w-auto max-w-full"
+          className="flex flex-col px-28  w-auto max-w-full "
           onSubmit={getOneRepo}
         >
           <input
@@ -63,21 +65,59 @@ export function SearchByRelated() {
             </button>
           </div>
         </form>
+      ) : (
+        <form
+          className="flex flex-col px-28 lg: h-[490px] md: h-[420px] sm: h-[380px] w-auto max-w-full "
+          onSubmit={getOneRepo}
+        >
+          <input
+            type="text"
+            value={repoName}
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              setRepoName(e.target.value)
+            }
+            className="rounded-lg py-3 border-gray-600 border-2 my-3 p-2 placeholder:text-gray-500 pl-3 shadow-md shadow-indigo-500/40 focus:outline-none"
+            placeholder="Pesquise por um repositório"
+          />
+          <input
+            type="text"
+            name="username"
+            id="username"
+            value={username}
+            onChange={(e: any) => setUserName(e.target.value)}
+            placeholder="Digite o nome do usuário"
+            className="rounded-lg py-3 border-gray-600 border-2 my-3 mb-6 p-2 placeholder:text-gray-500 pl-3 shadow-md shadow-indigo-500/40 focus:outline-none"
+          />
+          <div className="flex justify-center items-center">
+            <button
+              className="relative rounded-lg w-28 uppercase  text-sm font-medium p-3 transition duration-150 ease-in-out focus:outline-none focus:visible:ring-2 border-indigo-600 border-2 text-indigo-600 hover:bg-indigo-600 hover:text-white"
+              type="submit"
+            >
+              buscar
+            </button>
+          </div>
+        </form>
+      )}
 
-        <ul role="list" className="p-6 divide-y divide-slate-400">
-          {responseRepo?.map((items: any, index: number) => {
-            return (
-              <li
-                className="flex py-4 first:pt-0 last:pb-0 text-sm capitalize text-indigo-800"
-                key={index}
-              >
-                {items.name}
-              </li>
-            )
-          })}
-        </ul>
-        <Footer />
-      </div>
+      {responseRepo?.length > 0 && (
+        <h1 className="font-sans text-center uppercase w-full font-semibold text-indigo-800 lg:text-xl md:text-lg sm:text-[8px] p-10">
+          Resultado da busca: {repoName}
+        </h1>
+      )}
+
+      <ul role="list" className="p-6 divide-y divide-slate-400 ">
+        {responseRepo?.map((items: any, index: number) => {
+          return (
+            <li
+              className="flex py-4 first:pt-0 last:pb-0 text-sm capitalize text-indigo-800"
+              key={index}
+            >
+              {items.full_name}
+            </li>
+          )
+        })}
+      </ul>
+      <Footer />
     </>
   )
 }
